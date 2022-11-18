27a5158c2701f55ff5c31975ad9b70bd07a79af6
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2022-11-19 00:14:51 +0100

    working with configurations file

--- FirstApiCreated/Services/CloudMailService.cs
+++ FirstApiCreated/Services/CloudMailService.cs
@@ -2,8 +2,14 @@
 {
     public class CloudMailService : IMailService
     {
-        private String _mailTo = new string("Company@email.com");
-        private String _mailFrom = new string("NoReply@email.com");
+
+        private readonly String _mailTo = String.Empty;
+        private readonly String _mailFrom = String.Empty;
+        public CloudMailService (IConfiguration configuration)
+        {
+            _mailTo = configuration["MailService : ToMail"];
+            _mailFrom = configuration["MailService : FromMail"];
+        }
         public void Send(String subject, String message)
         {
             Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}  with {nameof(CloudMailService)} ");
