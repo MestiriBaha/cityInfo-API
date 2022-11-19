5a28014b981ad8733a3ec8e914a5a9e7d63964f0
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2022-11-19 12:18:15 +0100

    configuring the entities classes for storing data

---
+++ FirstApiCreated/Entities/City.cs
@@ -0,0 +1,28 @@
+﻿using FirstApiCreated.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+
+namespace FirstApiCreated.Entities
+{
+    public class City
+    {
+        //we want to make sure that each object city has a name ! 
+        // override the constructor !
+        public City(string name )
+        {
+            this.Name = name; 
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   
+        public int CityId { get; set; }
+        [Required]
+        [MaxLength(50)]
+        // we removed the by default string.empty not logical when it is required column !
+        public string Name { get; set; };
+        [MaxLength(200)] 
+        public string? Description { get; set; }
+      
+   //Many to one Relation !! 
+        public ICollection<pointsOfInterestDto> PointsofInterest { get; set; } = new List<pointsOfInterestDto>();
+    }
+}
---
+++ FirstApiCreated/Entities/PointOfInterest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
+;
+
+namespace FirstApiCreated.Entities
+   
+{
+    public class PointOfInterest
+    {
+        public PointOfInterest(string name)
+        {
+            Name = name;    
+        }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = String.Empty;
+        //One to many RELATION !! 
+        [ForeignKey("CityId")]
+        public City? City { get; set; }  
+    }
+}
