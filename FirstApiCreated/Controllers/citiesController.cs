b6081d51aae0112b3c45e5fb3c3232363b57578e
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2022-11-20 13:47:58 +0100

    using the Repository for controllers

--- FirstApiCreated/Controllers/citiesController.cs
+++ FirstApiCreated/Controllers/citiesController.cs
@@ -1,31 +1,46 @@
 ﻿using FirstApiCreated.Models;
 using Microsoft.AspNetCore.Mvc;
-using FirstApiCreated ; 
+using FirstApiCreated.Services ;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstApiCreated.Controllers
 {
+    
     [ApiController]
     [Route("api/[Controller]")]
     public class citiesController : ControllerBase
+
     {
-        private readonly citiesDataStore _cityDataStore; 
-        public citiesController(citiesDataStore cityDataStore)
+        //let 's get rid from the static data 
+        private readonly ICityInfoRepository _cityInfoRepository; 
+        public citiesController(ICityInfoRepository cityinforepository)
         {
-            _cityDataStore = cityDataStore;
+            _cityInfoRepository = cityinforepository ;
         }
     
         [HttpGet]
-        public ActionResult<cityDto> Getcities()
+        public async Task<ActionResult<IEnumerable<CityWithoutPointOfInterestDTO>>> Getcities()
         {
-            return Ok(_cityDataStore.cities);
-
+            var cities = await _cityInfoRepository.GetCitiesAsync();
+            IList<CityWithoutPointOfInterestDTO> result = new List<CityWithoutPointOfInterestDTO>();
+            foreach ( var city in cities)
+            {
+                result.Add(
+                    new CityWithoutPointOfInterestDTO
+                    {
+                        Name = city.Name,
+                        Id = city.CityId ,
+                        Description = city.Description 
+                    }) ; 
+            }
+            return Ok(result);
         }
         [HttpGet("{id}")]
 
         public ActionResult<cityDto> Getcity (int id) {
-           var result = _cityDataStore.cities.FirstOrDefault( ptr => ptr.Id==id);
-            if ( result==null) { return NotFound(); } ; 
-            return Ok(result);  
+           //var result = _cityDataStore.cities.FirstOrDefault( ptr => ptr.Id==id);
+           // if ( result==null) { return NotFound(); } ; 
+            return Ok();  
         }
 
     }
---
+++ FirstApiCreated/Models/CityWithoutPointOfInterestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstApiCreated.Models
+{
+    public class CityWithoutPointOfInterestDTO
+    { 
+        //let's follow the Demo so we get to his point !! 
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string? Description { get; set; }
+    }
+}
